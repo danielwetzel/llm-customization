5d7426a02baede00d433510e5c26e6bd42de6a08
Daniel Wetzel <daniel@d-wetzel.de>
2024-06-15 17:30:11 +0000

    llama-3-finetuning

--- init.sh
+++ init.sh
@@ -6,3 +6,5 @@ python3 -m pip install -r requirements.txt
 git config --global user.email "daniel@d-wetzel.de"
 git config --global user.name "Daniel Wetzel"
 
+git submodule init
+git submodule update
