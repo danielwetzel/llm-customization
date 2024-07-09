8c27b3447300f83924024b55cc88bc1864855986
Daniel Wetzel <daniel@d-wetzel.de>
2024-07-09 14:53:52 +0000

    Updated Init

--- init.sh
+++ init.sh
@@ -1,8 +1,6 @@
 #! /usr/bin/bash
 
-python3 -m conda init
-python3 -m conda activate pytorch
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 git config --global user.email "daniel@d-wetzel.de"
 git config --global user.name "Daniel Wetzel"
