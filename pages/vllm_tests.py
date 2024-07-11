9a3f4865eb9530402fc1c142d192ad3fa47183a1
Daniel Wetzel <daniel@d-wetzel.de>
2024-07-11 18:37:32 +0000

    Added vLLM and Transformers Comparison

--- pages/vllm_tests.py
+++ pages/vllm_tests.py
@@ -8,6 +8,101 @@ import sys
 from pages.utils.streamlit_utils import *
 
 
+def comp_page(df):
+
+    lables = [
+        '1: GPU Non-Idle',
+        '2: GPU Idle',
+        '0: GPU',
+        '3: CPU',
+        '4: RAM'
+    ]
+
+    default_labels = [
+        '1: GPU Non-Idle',
+        '2: GPU Idle',
+        '3: CPU',
+        '4: RAM'
+    ]
+
+    st.subheader("Inference Serving Engine / Framework Comparison")
+    st.write("This section visualized the energy consumption of vLLM compared to HuggingFace Transformers.")
+
+    st.write("")
+
+    with st.container(border=True):
+
+        label_selections = st.multiselect(label='Choose Energy Types', options=lables, default=default_labels, key='comp')
+
+        st.divider()
+
+        # Prepare the dataframe for the stacked bar chart
+        df_melted = df.melt(id_vars=[
+                                    'engine', 
+                                    'num_prompts', 
+                                    'model_type', 
+                                    'parameters', 
+                                    'total_time',
+                                    'time_per_prompt', 
+                                    'tok_per_sec',
+                                    'avg_out_tok', 
+                                    'total_out_tok'], 
+                            value_vars=[
+                                    'actual_non_idle_gpu_energy_per_10k_prompts',
+                                    'actual_idle_gpu_energy_per_10k_prompts', 
+                                    'actual_gpu_energy_per_10k_prompts',
+                                    'actual_cpu_energy_per_10k_prompts', 
+                                    'actual_ram_energy_per_10k_prompts'],
+                            var_name='Energy_Type', 
+                            value_name='Energy_Consumption')
+
+
+        # Define a sorting index for Energy_Type
+        df_melted['Energy_Type_Order'] = df_melted['Energy_Type'].map({
+            'actual_non_idle_gpu_energy_per_10k_prompts': '1: GPU Non-Idle',
+            'actual_idle_gpu_energy_per_10k_prompts': '2: GPU Idle',
+            'actual_gpu_energy_per_10k_prompts': '0: GPU',
+            'actual_cpu_energy_per_10k_prompts': '3: CPU',
+            'actual_ram_energy_per_10k_prompts': '4: RAM'
+        })
+
+        df_melted['display_colors'] = df_melted['Energy_Type_Order'].map({
+            '1: GPU Non-Idle': '#16c473',
+            '2: GPU Idle': '#5bf0d2',
+            '0: GPU': '#005239',
+            '3: CPU': '#9a86b8',
+            '4: RAM': '#86b7b8'
+        })
+
+        filtered_df = df_melted[df_melted['Energy_Type_Order'].isin(label_selections)]
+
+
+        # Stacked Bar Chart: Breakdown of Actual Energy Consumption
+        stacked_bar_chart = alt.Chart(filtered_df).mark_bar(size=50).encode(
+            x=alt.X('engine', title='Inference Engine / Framework'),
+            y=alt.Y('sum(Energy_Consumption)', title='Energy Consumed per 10k Prompts (in kWh)'),
+            color=alt.Color('Energy_Type_Order', title='Energy Type', 
+                            scale=alt.Scale(
+                                domain=label_selections,
+                                range=filtered_df['display_colors'].unique())
+                            ),
+            order=alt.Order('Energy_Type_Order', sort='descending'),
+            tooltip=['num_prompts', 'Energy_Type', 'Energy_Consumption']
+        ).properties(
+            width=600,
+            height=600,
+            title='Breakdown of Actual Energy Consumption per 10k Prompts'
+        )
+
+        st.altair_chart(stacked_bar_chart, use_container_width=True)
+
+
+    
+
+    
+    #st.write(df)
+
+    #st.write(filtered_df)
 
 
 
@@ -179,9 +274,16 @@ def vllm_tests():
     #st.subheader("", divider='grey')
 
     df = load_csv_data('emission_regression_vllm')
+    comp_df = load_csv_data('transformers_vs_vllm')
 
-    breackdown, regression = st.tabs(["Energy Type Breakdown", "Energy Regression"])
+    comp, breackdown, regression = st.tabs(["Transformers vs. vLLM","Energy Type Breakdown", "Energy Regression"])
     
+    with comp:
+        st.write("")
+        st.write("")
+
+        comp_page(comp_df)
+
     with breackdown:
 
         st.write("")
