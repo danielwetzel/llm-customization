4bbb1ca1a7e9b85bed0ce5f6c8acfebec9380a3c
Daniel Wetzel <daniel@d-wetzel.de>
2024-07-12 15:55:55 +0000

    AutoFP8-Quant

---
+++ load_autofp8.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# This messes with the Permissions inside the Instance - Only use this script if you want to quantize.
+
+git clone https://github.com/neuralmagic/AutoFP8.git
+pip install -e AutoFP8
